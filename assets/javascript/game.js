c2b6363c54f8bb9356dcb8849b7d96398964fcd9
elaine01 <ecumming.g@gmail.com>
2017-11-14 23:27:48 -0800

    cutting code, adding function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,43 +8,50 @@ var currentGuessed = []; // Your Guesses So Far: (the specific letters that the 
 
 // Full list of choices computer can choose from
 var computerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// characters user can't use
+var characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ",", ".", "/", ";", "[", "]", "|", "!", "@", "#", "$", "%", "^", "&", "*"," (", ")", "\\", "=", "+", "-", "_", "?", "<", ">"]
 
 // When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
-
 document.onkeyup = function(event) {
-
 
 	if (event.key === "Meta") {
 		console.log(event.key)
 		return;	
 	}
 
+// reset function
+var reset = function() {
+	guessesLeft = 9;
+	currentGuessed.length = 0;
+	document.querySelector(".guesses").innerHTML = "Guess left: 9";
+}
+
 	// User's & computer guess
 	var userGuess = event.key.toLowerCase();
 	var computerGuess = computerLetters[Math.floor(Math.random()*computerLetters.length)];
 
-	if (numbers.indexOf(userGuess) !== -1) {
+	if (characters.indexOf(userGuess) !== -1) {
 		return alert("You must pick a letter to play!");
 	} 
 
 	if (userGuess === computerGuess) { // if user guesses correctly
 		wins++;
 		console.log("User wins");
-		guessesLeft = 9;
-		currentGuessed.length = 0;
+		reset();
 		document.querySelector(".win").innerHTML = "Wins: " + wins;
 	} else if (guessesLeft === 0) { // if user has 0 guesses left and still guessed incorrectly, reset
 		losses++;
 		console.log("User loses");
-		guessesLeft = 9;
-		currentGuessed.length = 0;
+		reset();
 		document.querySelector(".loss").innerHTML = "Losses: " + losses;
 	} else if (userGuess !== computerGuess) { // if user guesses incorrectly, subtract 1 guess left
 		guessesLeft--;
+		console.log("guesses left")
+		document.querySelector(".guesses").innerHTML = "Guess Left: " + guessesLeft;
 	}
 	
+	// Display the letters that the user guessed on viewport
 	currentGuessed.push(userGuess);
-	document.querySelector(".alreadyGuessed").innerHTML = currentGuessed + " ";
-
+	document.querySelector(".alreadyGuessed").innerHTML = currentGuessed;
 	}
