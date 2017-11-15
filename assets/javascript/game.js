03f81beceb68eaa88018a847d3bf816828f08f5b
elaine01 <ecumming.g@gmail.com>
2017-11-14 18:17:17 -0800

    worked on logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,39 +8,54 @@ var currentGuessed = []; // Your Guesses So Far: (the specific letters that the 
 
 // Full list of choices computer can choose from
 var computerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 // When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
 
 document.onkeyup = function(event) {
 
+
+	if (event.key === "Meta") {
+		console.log(event.key)
+		return;	
+	}
+
+
 	// User's & computer guess
-	var userGuess = String.currentGuessed(event.key).toLowerCase();
+	var userGuess = event.key.toLowerCase();
 	var computerGuess = computerLetters[Math.floor(Math.random()*computerLetters.length)];
 
+	if (numbers.indexOf(userGuess) !== -1) {
+		return alert("You must pick a letter to play!");
+	} 
 
 	if (userGuess === computerGuess) { // if user guesses correctly
 		wins++;
 		console.log("User wins");
 		guessesLeft = 9;
 		currentGuessed.length = 0;
+		document.querySelector(".win").innerHTML = "Wins: " + wins;
 	} else if (guessesLeft === 0) { // if user has 0 guesses left and still guessed incorrectly, reset
 		losses++;
 		console.log("User loses");
 		guessesLeft = 9;
 		currentGuessed.length = 0;
-	} else if (userGuess !== computer) { // if user guesses incorrectly, subtract 1 guess left
+		document.querySelector(".loss").innerHTML = "Losses: " + losses;
+	} else if (userGuess !== computerGuess) { // if user guesses incorrectly, subtract 1 guess left
 		guessesLeft--;
 	}
 	
+	currentGuessed.push(userGuess);
+	document.querySelector(".alreadyGuessed").innerHTML = currentGuessed + " ";
 
-	var html = 
-	"<h1>The Psychic Game</h1>" +
-	"<p>Guess what letter I'm thinking of</p>" +
-	"<p>Wins: " + wins + " </p>" +
-	"<p>Losses: " + losses + "</p>" +
-	"<p>Guess Left: " + guessesLeft + "</p>" +
-	"<p>Your Guesses So Far: " + currentGuessed + "</p>";
+
+  
+	// "<h1>The Psychic Game</h1>" +
+	// "<p>Guess what letter I'm thinking of</p>" +
+	// "<p>Wins: " + wins + " </p>" +
+	// "<p>Losses: " + losses + "</p>" +
+	// "<p>Guess Left: " + guessesLeft + "</p>" +
+	// "<p>Your Guesses So Far: " + currentGuessed + "</p>";
 
 
 }
